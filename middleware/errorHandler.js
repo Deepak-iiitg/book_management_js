76490fb6406b986641df9872f1f0711113cf73b9
Deepak-iiitg <94068572+Deepak-iiitg@users.noreply.github.com>
2024-09-26 15:39:33 +0530

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (error,req,res,next)=>{
     console.log(error.stack || error.message);
+    
     return res.status(error.status || 500).json({message:error.message || 'Internal server error'});
 }
 
